86656ee44e9fa737850fbee823227e05caaf3371
SweeRoty <sweeroty@gmail.com>
2020-08-17 14:38:54 +0800

    change the location of the config file

--- app/vertices/extractRawVertices.py
+++ app/vertices/extractRawVertices.py
@@ -47,7 +47,7 @@ def transform_to_row(t):
 if __name__ == '__main__':
 	print('====> Initializing Spark APP')
 	localConf = RawConfigParser()
-	localConf.read('./config')
+	localConf.read('../../config')
 	sparkConf = SparkConf()
 	for t in localConf.items('spark-config'):
 		sparkConf.set(t[0], t[1])
--- app/vertices/prepareVertices.py
+++ app/vertices/prepareVertices.py
@@ -27,7 +27,7 @@ def getRawRecords(spark, fr, to):
 if __name__ == '__main__':
 	print('====> Initializing Spark APP')
 	localConf = RawConfigParser()
-	localConf.read('./config')
+	localConf.read('../../config')
 	sparkConf = SparkConf()
 	for t in localConf.items('spark-config'):
 		sparkConf.set(t[0], t[1])
