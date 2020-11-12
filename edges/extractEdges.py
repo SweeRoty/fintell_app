cce5594a9078d0d3ac88e5af28af021aa9103137
SweeRoty <sweeroty@gmail.com>
2020-11-12 12:43:14 +0800

    change the path of edge computation and add parameters to make the script more flexible

--- edges/extractEdges.py
+++ edges/extractEdges.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from calendar import monthrange
 from ConfigParser import RawConfigParser
 from pyspark import SparkConf
 from pyspark.sql import functions as F
@@ -23,7 +22,7 @@ def getInvalidDevices(spark, data_date):
 	devices = spark.sql(sql)
 	return devices
 
-def loadSampledDevices(spark, query_month):
+def loadSampledDevices(spark, device_date):
 	sql = """
 		select
 			imei
@@ -31,7 +30,7 @@ def loadSampledDevices(spark, query_month):
 			ronghui.hgy_01
 		where
 			data_date = '{0}'
-	""".format(query_month)
+	""".format(device_date)
 	print(sql)
 	devices = spark.sql(sql)
 	return devices
@@ -55,7 +54,7 @@ def getRawEdges(spark, fr, to):
 	edges = spark.sql(sql)
 	return edges
 
-def getVertices(spark, query_month):
+def getVertices(spark, data_date):
 	sql = """
 		select
 			app_package,
@@ -65,42 +64,39 @@ def getVertices(spark, query_month):
 			ronghui.hgy_05
 		where
 			data_date = '{0}'
-	""".format(query_month)
+	""".format(data_date)
 	print(sql)
 	vertices = spark.sql(sql)
 	return vertices
 
 def generateEdge(t):
+	global gamma, penalty, seed
 	import random, time
 	apps = sorted(t[1], key=lambda t: t[0])
 	n = len(apps)
 	edges = []
-	seed = int(time.time())%202005
+	seed = int(time.time())%seed
 	count = 0
 	if n > 1:
 		for i in range(n-1):
 			for j in range(i+1, n):
-				#if count > 9:
-				#	break
-				#edge_prob = 1e7/(apps[i][0]*apps[j][0])
-				edge_prob = 1.1e4/(np.power(apps[i][1], 0.6)*np.power(apps[j][1], 0.6))
-				#edges.append(('{0}_{1}'.format(apps[i][0], apps[j][0]), edge_prob))
+				edge_prob = gamma/(np.power(apps[i][1], penalty)*np.power(apps[j][1], penalty))
 				random.seed(seed+count)
 				if random.random() < edge_prob:
-					edge_name = '{0}sweeroty{1}'.format(apps[i][0], apps[j][0])
+					edge_name = '{0}_rlab_{1}'.format(apps[i][0], apps[j][0])
 					edges.append(edge_name)
 				count += 1
 	return edges
 
 def transform_to_row(t):
-	fr, to = t[0].split('sweeroty')
+	fr, to = t[0].split('_rlab_')
 	return Row(fr=fr, to=to, weight=int(t[1]))
 
 if __name__ == '__main__':
 	print('====> Initializing Spark APP')
 	localConf = RawConfigParser()
 	localConf.optionxform = str
-	localConf.read('../../config')
+	localConf.read('../config')
 	sparkConf = SparkConf()
 	for t in localConf.items('spark-config'):
 		sparkConf.set(t[0], t[1])
@@ -116,25 +112,32 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--fr', type=str)
 	parser.add_argument('--to', type=str)
-	parser.add_argument('--device_date', type=str)
+	parser.add_argument('--freq_thres', type=int)
+	parser.add_argument('--gamma', type=float, default=7.5e3)
+	parser.add_argument('--penalty', type=float, default=0.75)
+	parser.add_argument('--seed', type=int, default=198910)
 	args = parser.parse_args()
-	query_month = args.fr[:6]
-	month_end = query_month+str(monthrange(int(query_month[:4]), int(query_month[4:]))[1])
+	gamma = args.gamma
+	penalty = args.penalty
+	seed = args.seed
 
-	print('====> Start calculation')
+	print('====> Start computation')
 	edges = getRawEdges(spark, args.fr, args.to)
-	"""
-	devices = getInvalidDevices(spark, month_end)
-	edges = edges.join(devices, on=['imei'], how='left_outer').where(F.col('flag').isNull())
-	"""
-	devices = loadSampledDevices(spark, args.device_date).sample(False, 0.05, 11267)
+	devices = loadSampledDevices(spark, args.fr).sample(False, 0.05, 11267)
 	edges = edges.join(devices, on=['imei'], how='inner')
 	vertices = getVertices(spark, args.fr)
-	vertices = vertices.where(vertices.app_freq > 27000)
+	index_thres = vertices.where(F.col('app_freq') <= args.freq_thres) \
+							.select(F.max('app_index').alias('index_thres')) \
+							.rdd \
+							.map(lambda row: row['index_thres']) \
+							.collect()[0]
+	vertices = vertices.where(F.col('app_freq') > args.freq_thres)
 	edges = edges.join(vertices, on=['app_package'], how='inner').drop('app_package')
-	#edges = edges.where(edges.app_freq > 27000)
-	edges = edges.repartition(30000, 'imei').rdd.map(lambda row: (row['imei'], (row['app_index']-17477003, row['app_freq']-27000))).groupByKey().flatMap(generateEdge)
+	edges = edges.repartition(30000, 'imei') \
+					.rdd \
+					.map(lambda row: (row['imei'], (row['app_index']-index_thres, row['app_freq']-args.freq_thres))) \
+					.groupByKey() \
+					.flatMap(generateEdge)
 	edges = edges.map(lambda t: (t, 1)).reduceByKey(lambda x, y: x+y).map(transform_to_row).toDF()
 	edges = edges.select('fr', 'to', 'weight').registerTempTable('tmp')
 	spark.sql('''INSERT OVERWRITE TABLE ronghui.hgy_07 PARTITION (data_date = '{0}') SELECT * FROM tmp'''.format(args.fr)).collect()
-	#edges.repartition(1).write.csv('/user/hive/warehouse/ronghui_bj.db/hgy/app/edges/{0}_7days_freq_11000_0.6'.format(args.fr), header=True)
