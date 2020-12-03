65b189311cfaa7ad827b43df91d772529e9bc898
SweeRoty <sweeroty@gmail.com>
2020-12-03 14:56:28 +0800

    make input_path, output_path and seed as program parameters

--- samples/experiments/sampling.py
+++ samples/experiments/sampling.py
@@ -1,15 +1,23 @@
 # -*- coding: utf-8 -*-
 
+import argparse
+
 import numpy as np
 import pandas as pd
 
 if __name__ == '__main__':
-	samples = pd.read_csv('./samples_0901_2799.csv', usecols=['app_package', 'app_index', 'class_one', 'flag_debt', 'flag_714'])
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--input', type=str)
+	parser.add_argument('--output', type=str)
+	parser.add_argument('--seed', type=int, default=186)
+	args = parser.parse_args()
+
+	samples = pd.read_csv(args.input, usecols=['app_package', 'app_index', 'class_one', 'flag_debt', 'flag_714'])
 	samples['label'] = samples[['flag_debt', 'flag_714']].any(axis=1)
 	samples.loc[samples.class_one.isin(['购物优惠', '金融理财']) & (samples.label != 1), 'label'] = -1
 	samples.loc[samples.class_one.isna() & (samples.label != 1), 'label'] = -2
 
-	np.random.seed(186)
+	np.random.seed(args.seed)
 	samples['seed'] = np.random.rand(samples.shape[0])
 	samples['group_index'] = samples.groupby('label')['seed'].rank('dense')
 	samples['phase'] = -1
@@ -24,4 +32,4 @@ if __name__ == '__main__':
 	samples.sort_values('app_index', inplace=True)
 	samples['app_index'] = np.arange(1, samples.shape[0]+1)
 	samples.loc[samples.label < 0, 'label'] = False
-	samples[['app_package', 'app_index', 'label', 'phase']].to_csv('./final_samples_0901_2799.csv', index=False)
+	samples[['app_package', 'app_index', 'label', 'phase']].to_csv(args.output, index=False)
