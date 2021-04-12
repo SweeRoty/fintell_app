84556f67e1983d154573fa1903070f1b2856d330
SweeRoty <sweeroty@gmail.com>
2021-04-12 15:06:57 +0800

    remove data_date and get flags for debt and 714 APPs

--- vertices/retrieveSamples.py
+++ vertices/retrieveSamples.py
@@ -65,9 +65,15 @@ if __name__ == '__main__':
 	print('====> Start computation')
 	vertices = getVertices(spark, args.data_date, args.thres)
 	labels = getAppCategories(spark)
-	vertices = vertices.join(labels, on='app_package', how='left_outer')
+	vertices = vertices.join(labels, on='app_package', how='left_outer').drop('data_date')
+	apps_debt = spark.read.csv('/user/ronghui_safe/hgy/app/manual_labeling/debt_app.csv', sep='\t', header=True)
+	apps_debt = apps_debt.select('app_package').distinct().rdd.map(lambda row: row['app_package']).collect()
+	vertices = vertices.withColumn('flag_debt', F.when(vertices.app_package.isin(apps_debt), 1).otherwise(0))
+	apps_714 = spark.read.csv('/user/ronghui_safe/hgy/app/manual_labeling/714.csv', sep='\t', header=True)
+	apps_714 = apps_714.select('app_package').distinct().rdd.map(lambda row: row['app_package']).collect()
+	vertices = vertices.withColumn('flag_714', F.when(vertices.app_package.isin(apps_714), 1).otherwise(0))
 	if args.verbose:
 		class_stats = vertices.rdd.map(lambda row: (row['class_one'].encode('utf-8') if row['class_one'] is not None else 'NA', 1)).reduceByKey(add).collect()
 		for cl, count in class_stats:
 			print("-----Category {0}'s count is {1}".format(cl, count))
-	vertices.repartition(1).write.csv('/user/ronghui_safe/hgy/app/samples/samples_with_label_{0}'.format(args.data_date), header=True)
+	vertices.repartition(1).write.csv('/user/ronghui_safe/hgy/app/samples/samples_with_label_{0}_{1}'.format(args.data_date, args.thres), header=True)
